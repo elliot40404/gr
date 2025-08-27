a1b28d78ec1b859b04a7548c498ffab4c5948300
Elliot <67360013+elliot40404@users.noreply.github.com>
2025-08-28 01:01:02 +0530

    improved api

--- main.go
+++ main.go
@@ -4,108 +4,120 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"slices"
 )
 
-func checkSecondaryArgs(args []string) int {
-	for i, arg := range args {
-		if arg == "--" {
-			return i
-		}
-	}
-	return -1
-}
+func findCmdBinaries() ([]string, error) {
+	binaries := []string{}
+	cmdPath := "./cmd"
 
-func checkBin(args []string) string {
-	for i, arg := range args {
-		if arg == "--bin" {
-			if i+1 < len(args) {
-				return args[i+1]
+	if _, err := os.Stat(cmdPath); os.IsNotExist(err) {
+		return binaries, nil
+	}
+
+	files, err := os.ReadDir(cmdPath)
+	if err != nil {
+		return nil, fmt.Errorf("error: could not read 'cmd' directory: %w", err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			if _, err := os.Stat(filepath.Join(cmdPath, file.Name(), "main.go")); err == nil {
+				binaries = append(binaries, file.Name())
 			}
 		}
 	}
-	return ""
+	return binaries, nil
+}
+
+func run(path string, args ...string) {
+	cmdArgs := []string{"run", path}
+	cmdArgs = append(cmdArgs, args...)
+
+	cmd := exec.Command("go", cmdArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	err := cmd.Run()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			os.Exit(exitError.ExitCode())
+		}
+		fmt.Printf("Error: Failed to execute command: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func printUsage(binaries []string) {
+	fmt.Println("Multiple binaries found. Please specify which one to run.")
+	fmt.Println("\nAvailable binaries:")
+	for _, bin := range binaries {
+		fmt.Printf("  %s\n", bin)
+	}
+	fmt.Println("\nUsage:")
+	fmt.Println("  gr <binary_name> [args_for_program...]")
+	fmt.Println("  gr --bin <binary_name> [args_for_program...]")
 }
 
 func main() {
 	args := os.Args[1:]
-	var secondaryArgs []string
-	if len(args) > 0 {
-		index := checkSecondaryArgs(args)
-		if index != -1 {
-			secondaryArgs = args[index+1:]
-			args = args[:index]
-		}
-	}
-	bin := checkBin(args)
-	// if args do not exist secondaryArgs = Args
-	if len(secondaryArgs) == 0 {
-		secondaryArgs = args
-	}
-	// check if main.go exists
-	if _, err := os.Stat("main.go"); !os.IsNotExist(err) {
-		run([]string{"go", "run", "."}, secondaryArgs...)
-	} else {
-		// look for cmd directory
-		if _, err := os.Stat("cmd"); !os.IsNotExist(err) {
-			//list all directories in cmd
-			files, err := os.ReadDir("cmd")
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			binaries := make([]string, 0)
-			for _, file := range files {
-				if file.IsDir() {
-					binaries = append(binaries, file.Name())
-				}
-			}
-			switch len(binaries) {
-			case 0:
-				fmt.Println("No binaries found in cmd directory")
-				return
-			case 1:
-				run([]string{"go", "run", fmt.Sprintf("./cmd/%s/", binaries[0])}, secondaryArgs...)
-				return
-			default:
-				if bin != "" {
-					found := false
-					for _, binary := range binaries {
-						if binary == bin {
-							found = true
-							run([]string{"go", "run", fmt.Sprintf("./cmd/%s/", binary)}, secondaryArgs...)
-							break
-						}
-					}
-					if !found {
-						fmt.Println("Binary not found in cmd directory")
-					}
-				} else {
-					fmt.Println("Multiple binaries found in cmd directory, please specify which one to run")
-					fmt.Println("Available binaries:")
-					for _, binary := range binaries {
-						fmt.Println(binary)
-					}
-					fmt.Println("Usage: gr --bin <binary_name> [args]")
-				}
-			}
-		} else {
-			fmt.Println("Could not find main.go or cmd directory")
-		}
-	}
-}
 
-func run(inp []string, args ...string) {
-	if len(inp) == 0 {
-		fmt.Println("No command provided")
+	if _, err := os.Stat("main.go"); err == nil {
+		run(".", args...)
 		return
 	}
-	inp = append(inp, args...)
-	cmd := exec.Command(inp[0], inp[1:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-	err := cmd.Run()
+
+	binaries, err := findCmdBinaries()
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
+
+	if len(binaries) == 0 {
+		fmt.Println("Error: No 'main.go' file or runnable 'cmd/' packages found.")
+		os.Exit(1)
+	}
+
+	var binToRun string
+	var programArgs []string
+
+	if len(args) > 0 {
+		firstArg := args[0]
+		isBinaryName := slices.Contains(binaries, firstArg)
+
+		if firstArg == "--bin" {
+			if len(args) > 1 {
+				binToRun = args[1]
+				programArgs = args[2:]
+			}
+		} else if isBinaryName {
+			binToRun = firstArg
+			programArgs = args[1:]
+		} else {
+			programArgs = args
+		}
+	} else {
+		programArgs = []string{}
+	}
+
+	if binToRun == "" {
+		if len(binaries) == 1 {
+			binToRun = binaries[0]
+		} else {
+			printUsage(binaries)
+			os.Exit(1)
+		}
+	}
+
+	isValid := slices.Contains(binaries, binToRun)
+
+	if !isValid {
+		fmt.Printf("Error: Binary '%s' not found in 'cmd' directory.\n", binToRun)
+		printUsage(binaries)
+		os.Exit(1)
+	}
+
+	run(fmt.Sprintf("./cmd/%s/", binToRun), programArgs...)
 }
